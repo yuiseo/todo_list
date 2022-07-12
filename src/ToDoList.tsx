9fe751c4d776aa42121f984646fa30af6602fec9
유이서 <yoojh456@naver.com>
2022-07-13 04:44:53 +0900

    form validation(react-hook-form 사용)

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
-
+import {useForm} from "react-hook-form";
+/*
 function ToDoList(){
   const [todo,setTodo] = useState("");
   const onChange = (event:React.FormEvent<HTMLInputElement>) => {
@@ -19,5 +20,32 @@ function ToDoList(){
     </form>
   </div>;
 }
+*/
 
+function ToDoList(){
+  const { register, watch,handleSubmit, formState } = useForm();
+  const onValid = (data:any) => {
+    console.log(data)
+  };
+  console.log(formState.errors);
+  return <div>
+    <form style = {{display:"flex",flexDirection: "column"}} onSubmit={handleSubmit(onValid)}>
+      <input {...register("Email",{ required: true})} placeholder='Email'/>
+      <input {...register("firstName",{ required: true})} placeholder='First Name'/>
+      <input {...register("lastName",{ required: true})} placeholder='Last Name'/>
+      <input {...register("username",{ required: true, minLength:10})} placeholder='Username'/>
+      <input {...register("password",{ required: true,minLength:5})} placeholder='Password'/>
+      <input 
+      {...register("password1",{ 
+        required: "password is required",
+        minLength:{
+          value:5,
+          message:"your password is too short."
+      },
+      })} 
+      placeholder='Password1'/>
+      <button>Add</button>
+    </form>
+  </div>;
+}
 export default ToDoList;
