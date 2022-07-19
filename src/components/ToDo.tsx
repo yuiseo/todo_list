7773f4934d4a971c330d415fc865386a2694cae9
yuiseo <24oesiu@gmail.com>
2022-07-20 00:59:05 +0900

    finish

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { useSetRecoilState } from 'recoil';
-import { IToDo, toDoState } from './atoms';
+import { Categories, IToDo, toDoState } from './atoms';
 
 
-const food = ["pizza","mango","kimchi","kimbob"]
-const front = ["pizza"]
-const back = ["kimchi","kimbob"]
-const finalPart = [...front,"감",...back]
+
 function ToDo({text,category,id}:IToDo){
   const setToDos = useSetRecoilState(toDoState)
   const onClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
@@ -23,13 +20,13 @@ function ToDo({text,category,id}:IToDo){
       ];
     })
   };
-  
+
   return(
   <li>
     <span>{text} </span>
-    {category !== "DOING" &&<button name="DOING" onClick={onClick}>Doing</button>}
-    {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
-    {category !== "DONE" &&<button name="DONE" onClick={onClick}>Done</button>}
+    {category !== Categories.DOING &&<button name={Categories.DOING+""} onClick={onClick}>Doing</button>}
+    {category !== Categories.TO_DO && <button name={Categories.TO_DO+""} onClick={onClick}>To Do</button>}
+    {category !== Categories.DONE &&<button name={Categories.DONE+""} onClick={onClick}>Done</button>}
 
   </li>);
 }
